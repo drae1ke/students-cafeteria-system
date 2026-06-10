e5456470bb496aa61e6b09bbc19f051fde2b4e09
drae1ke <phelleeks@gmail.com>
2026-06-10 13:42:31 +0300

    cors configuration

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000',
-    'https://e78e-217-199-148-231.ngrok-free.app'
+    'https://e78e-217-199-148-231.ngrok-free.app',
+    'https://studentscafe.onrender.com/'
 ];
 module.exports = allowedOrigins;
